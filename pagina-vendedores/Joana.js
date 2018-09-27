9fc7fcf74c7d70f35854a7c46e4756e320a0f939
Fernando Antonio <fernando.acouto@outlook.com>
2018-09-27 16:54:26 -0300

    Bom appetit versao dia 27/09/2018, layout melhorado nas paginas dos vendedores

--- pagina-vendedores/Joana.js
+++ pagina-vendedores/Joana.js
@@ -3,19 +3,26 @@ var app = new Vue ({
     data: {
         nome: "Joana Souza",
         tipo_brigadeiro: "vazio",
-        quantidade_brigadeiro: "0",
+        quantidade_brigadeiro: 0,
         mensagem_share: "",
+        valor_brigadeiro: 1,
+        valor_total: 0, 
         link: "https://api.whatsapp.com/send?phone=55091984346208&text=" + this.mensagem_share,
         count: 0
     },
     created: function() {
         this.renderItem();
+        
+    },
+    computed: {
+        
     },
     methods: {
         renderItem: function() {
             var app = this;
-            this.mensagem_share =  "Gostaria de "+ this.quantidade_brigadeiro +" brigadeiro(s) de " + this.tipo_brigadeiro + ".";
+            this.mensagem_share =  "Olá, "+this.nome+". Gostaria de "+ this.quantidade_brigadeiro +" brigadeiro(s) de " + this.tipo_brigadeiro + ". O total seria R$"+ this.valor_total +". Vamos nos encontrar?";
         }
     }
     
 })
+
